73a028896235b296ffa54fff6e6b7c0d77cc8b7f
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-11-21 12:23:03 +0800

    Update GenRandArr.cpp

--- src/GenRandArr.cpp
+++ src/GenRandArr.cpp
@@ -1,2 +1,14 @@
-int *generateRandomArray(int arraysize, int rangeLeft, int rangeRight)
+int *generateRandomArray(int arraysize, int rangeLeft, int rangeRight){
+  assert(rangeLeft <= rangeRight); //区间边界输入错误,中断程序
+  
+  int *arr = new int[n];  创建长度为n的数组
+  
+  srand(time(NULL)); //生成随机种子
+  
+  for(int i = 0; i < n; i++)
+  {
+    arr[i] = rand() % (rangeRight - rangeLeft  + 1) + rangeLeft;
+  }
+  return arr;
+}
 
