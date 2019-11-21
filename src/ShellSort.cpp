2b56367a1ee4861d56bb05b65f445340de8d624f
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-11-21 14:01:44 +0800

    Create ShellSort.cpp
    My implement of Shell sort reference from "Data Structures and Algorithm Analysis in C"

---
+++ src/ShellSort.cpp
@@ -0,0 +1,19 @@
+void ShellSort(int arr[], int N){
+  int i,j,Increament;
+  int Tmp;
+  
+  
+  for(Increament = N / 2; Increament > 0; Increament /= 2){
+    for( i = Increament; i < N; i++)
+    {
+      Tmp = arr[i];
+      for(j = i; j >= Increament; j -= Increament)
+      {
+        if(Tmp < arr[j - Increament])
+          arr[j] = arr[j - Increament];
+        else
+          break;
+      }
+      arr[j] = Tmp;
+    }
+}
