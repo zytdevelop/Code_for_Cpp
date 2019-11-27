017e1bd9858ebe5f32d1b07a6a159c97535a0940
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-11-27 23:07:26 +0800

    Create FindPivotIndex.cpp
    LeetCode No.724 Find Pivot Index

---
+++ LeetCode/FindPivotIndex.cpp
@@ -0,0 +1,10 @@
+int pivotIndex(vector<int>& nums) {
+    int sum = 0, leftsum = 0;
+    for(int i = 0; i < nums.size(); i++)sum += nums[i];
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(leftsum == sum - leftsum - nums[i]) return i;
+        leftsum += nums[i];
+    }
+    return -1;
+}
