ab90977b9dbc9c7ac208a381fcd39ac3768f0f72
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-11-27 23:17:20 +0800

    Update FindPivotIndex.cpp

--- LeetCode/FindPivotIndex.cpp
+++ LeetCode/FindPivotIndex.cpp
@@ -1,3 +1,6 @@
+//解题思路:求出整个数组的和,再用总和减去从左到右累加和,如果得到的差和累加和相等,那么此时的索引就是所求的目标索引
+
+
 int pivotIndex(vector<int>& nums) {
     int sum = 0, leftsum = 0;
     for(int i = 0; i < nums.size(); i++)sum += nums[i];
