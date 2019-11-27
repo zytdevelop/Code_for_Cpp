67732c4497069ef95af76d0cfee33938049f23c0
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-11-27 11:09:01 +0800

    Update TwoSumII.cpp

--- LeetCode/TwoSumII.cpp
+++ LeetCode/TwoSumII.cpp
@@ -1,3 +1,5 @@
+//官方解答:利用双指针的原因是,数组已被排序.那么首位两数相加,如果结果大于目标值,那么尾部元素前移,进行寻找;如果结果小于目标值,头部元素后移进行寻找.
+
 vector<int> twoSum(vector<int>& numbers, int target) {
     int low = 0, high = numbers.size() -1;
     while(low < high)
