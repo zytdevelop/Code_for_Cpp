44414c658a8092799f5547a2a60da51ef4bfad6f
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-12-08 16:28:14 +0800

    Create MaxConsecutiveOnes.cpp
    LeeCode No.485 Given a binary array, find the maximum number of consecutive 1s in this array.

---
+++ LeetCode/MaxConsecutiveOnes.cpp
@@ -0,0 +1,17 @@
+//思路:用临时变量max记录每个连续1的数,每个循环中计数变量count都与max进行比较,取其中的最大值
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+  int count = 0;
+  int max = 0;
+  
+  for(int i = 0; i < nums.size(); ++i)
+  {
+    if(nums.[i])
+      count++;
+    else
+      count = 0;
+    max = max > count ? max : count;
+  }
+  return max;
+
+}
