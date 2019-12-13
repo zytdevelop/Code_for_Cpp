b1e204a32ee76e40c8e8a4ecf9d1caac7bfe34da
zytdevelop <35646364+zytdevelop@users.noreply.github.com>
2019-12-13 15:01:20 +0800

    Update Merge2List.cpp

--- LeetCode/Merge2List.cpp
+++ LeetCode/Merge2List.cpp
@@ -1,3 +1,6 @@
+/*merge two sorted lists*/
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,7 +15,18 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     {
         return l1 == nullptr ? l2 : l1;
     }
-
+/**使用递归来进行链表的合并**/
+  if(l1->val < l2->val)
+  {
+    l1->next = mergeTwoLists(l1->next,l2);
+    return l1;
+  }
+  else
+  {
+    l2->next = mergeTwoLists(l1,l2->next);
+    return l2;
+  }
+/*使用循环进行链表的合并
     ListNode* newNode = new ListNode(0);
     ListNode* pNode = newNode;
     while(l1&&l2)
@@ -43,4 +57,6 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     }
 
     return newNode->next;
+    
+*/
 }
